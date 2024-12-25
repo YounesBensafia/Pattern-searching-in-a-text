ceec1611346cf85db4842373d7b5da7cbfd48b53
YounesBensafia <younes.bensafia@etu.usthb.dz>
2024-12-25 11:44:48 +0100

    Add memory tracking for suffix array, suffix tree, and FM index searches

--- testAll.py
+++ testAll.py
@@ -1,7 +1,7 @@
 from done.AlgoTree import SuffixTree
 from done.FmIndex import FMIndex
 from done.suffix_array import search_pattern_with_suffix_array
-from tracking_usage import track_time
+from tracking_usage import track_time, track_memory
 
 
 text = "bananananananabananabanananaannnnnnnnnnnnnnnnnnnnnnhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
@@ -11,25 +11,33 @@ pattern = "na"
 
 
 time_suffix_array = track_time(lambda: search_pattern_with_suffix_array(text, pattern))
+memory_suffix_array = track_memory(lambda: search_pattern_with_suffix_array(text, pattern))
+
 indices = search_pattern_with_suffix_array(text, pattern)
-print("TIME IS: ", time_suffix_array)
-print(f"Positions: {indices}")
+
+print("\n================== Suffix Array ==================\n")
+print(f"Time taken to search using Suffix Array: {time_suffix_array} seconds")
+print(f"Pattern found at positions: {indices}")
+print(f"Memory used for Suffix Array search: {memory_suffix_array} KB")
 
 
-print("===========================================================")
 
 suffix_tree = SuffixTree(text)
 time_exists = track_time(lambda: suffix_tree.find_substring(pattern))
 exists = suffix_tree.find_substring(pattern)
+memory_exists = track_memory(lambda: suffix_tree.find_substring(pattern))
 
 time_positions = track_time(lambda: suffix_tree.find_all_occurrences(pattern))
 positions = suffix_tree.find_all_occurrences(pattern)
+memory_positions = track_memory(lambda: suffix_tree.find_all_occurrences(pattern))
 
 total_time = time_exists + time_positions
-print("TIME IS: ", total_time)
+total_memory = memory_exists + memory_positions
 
-print(f"Positions: {positions}")
-print("===========================================================")
+print("\n================== Suffix Tree ==================\n")
+print(f"Time taken to search using Suffix Tree: {total_time} seconds")
+print(f"Pattern found at positions: {positions}")
+print(f"Memory used for Suffix Tree search: {total_memory} KB")
 
 # ===========================================================
 
@@ -37,15 +45,21 @@ fm = FMIndex(text)
 
 time = track_time(lambda: fm.count(pattern))
 count = fm.count(pattern)
+memory = track_memory(lambda: fm.count(pattern))
 
 time = time + track_time(lambda: fm.locate(pattern))
 positions = fm.locate(pattern)
+memory = memory + track_memory(lambda: fm.locate(pattern))
 
-print("TIME IS: ", time)
+print("\n================== FM Index ==================\n")
 
-print(f"Positions : {positions}")
+print(f"Time taken to search using FM Index: {time} seconds")
+print(f"Pattern found at positions: {positions}")
+print(f"Memory used for FM Index search: {memory} KB")
 
-print("===========================================================")
+
+# SUFFIX ARRAY > SUFFIX TREE > FM_INDEX
+
 
 
 
